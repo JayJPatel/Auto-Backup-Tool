e764cea63eb3bb960fdf769952edd5626774f0b9
Jay <j.patel8797@gmail.com>
2021-09-03 15:13:26 -0700

    Added class Backup

---
+++ src/backup.py
@@ -0,0 +1,15 @@
+
+#---------------------------------------------------------------------------
+# CLASS - Backup
+
+class Backup() : 
+    def __init__(self) : 
+        pass
+
+    def check(self) : 
+        pass
+
+    def run(self) : 
+        pass 
+
+    
