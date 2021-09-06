66c939d1eb958ea15d9f42955d128e9f394b6cb2
Jay <j.patel8797@gmail.com>
2021-09-05 17:11:28 -0700

    Added check for config.json in constructor

--- src/backup.py
+++ src/backup.py
@@ -1,10 +1,22 @@
+import os
+import json 
+import tkinter as tk 
+from tkinter import filedialog
+
+tkWin = tk.Tk() 
+tkWin.withdraw() 
 
 #---------------------------------------------------------------------------
 # CLASS - Backup
 
 class Backup() : 
     def __init__(self) : 
-        pass
+        if (os.path.isfile('config.json')) :
+            self.config_Exists = True 
+            self.parse()
+        else :
+            self.config_Exists = False  
+            print("\n config.json Not detected! Run setup before running backup")
 
     def check(self) : 
         pass
@@ -12,4 +24,8 @@ class Backup() :
     def run(self) : 
         pass 
 
-    
+    def parse(self) : 
+        pass 
+
+    def setup(self) : 
+        pass
