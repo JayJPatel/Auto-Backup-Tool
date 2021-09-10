887824feb9eb5f11b7e9acd0903bbdd7b52d588f
Jay <j.patel8797@gmail.com>
2021-09-10 15:36:51 -0700

    Added method getPath + updated setup

--- src/backup.py
+++ src/backup.py
@@ -1,10 +1,7 @@
 import os
-import json 
-import tkinter as tk 
+import json
+import tkinter as tk
 from tkinter import filedialog
-
-tkWin = tk.Tk() 
-tkWin.withdraw() 
 
 #---------------------------------------------------------------------------
 # CLASS - Backup
@@ -16,7 +13,7 @@ class Backup() :
             self.parse()
         else :
             self.config_Exists = False  
-            print("\n config.json Not detected! Run setup before running backup")
+            print("\nconfig.json Not detected! Run setup before running backup")
 
     def check(self) : 
         pass
@@ -27,5 +24,37 @@ class Backup() :
     def parse(self) : 
         pass 
 
+    # Prompt user for backup type, location, and files to backup
     def setup(self) : 
-        pass
+        os.system('cls')
+        print("---Setup---")
+
+        # Choose backup folder destination 
+        input("\nPress Enter to choose file path for destination folder")
+        #self.destination = filedialog.askdirectory() + "/Backup/"
+        self.destination = self.getPath("/Backup/")
+        print("Target destination directory: ", self.destination)
+
+        #TODO Ask if this directory is correct 
+
+        # Choose source folder location 
+        input("\nPress Enter to choose file path for source folder")
+        self.source = self.getPath("")
+
+        print("Target source directory: ", self.source)
+
+        input("\nPress Enter to return to the main menu...")
+
+    # Method that grabs file path -- used in setup 
+    def getPath(self, dirFolder) : 
+        # Create + hide tkinter window
+        tkWin = tk.Tk()
+        tkWin.wm_attributes("-topmost", 1) # Move window to front
+        tkWin.withdraw() 
+
+        # Open file dialog to retrieve path 
+        path = filedialog.askdirectory() + dirFolder
+
+        # Destroy tkinter window, and return path
+        tkWin.destroy()
+        return path 
