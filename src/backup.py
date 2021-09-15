63910a2f55d5b9ba6779aa790b36ee808e7a4bd3
Jay <j.patel8797@gmail.com>
2021-09-15 02:35:47 -0700

    Updated destination functionality in setup

--- src/backup.py
+++ src/backup.py
@@ -26,24 +26,69 @@ class Backup() :
 
     # Prompt user for backup type, location, and files to backup
     def setup(self) : 
-        os.system('cls')
-        print("---Setup---")
+        # Check if config exists 
+        if (self.config_Exists) : 
+            self.parse()
+        else : 
+            # If config doesnt exist, set false flags 
+            self.destination_Flag = False 
+            self.source_Flag = False
 
-        # Choose backup folder destination 
-        input("\nPress Enter to choose file path for destination folder")
-        #self.destination = filedialog.askdirectory() + "/Backup/"
-        self.destination = self.getPath("/Backup/")
-        print("Target destination directory: ", self.destination)
+        # Loop for setting folder paths 
+        while(True) :
+            # Output menu 
+            os.system('cls')
+            print("---Setup---")
+            print("\n1. Choose file path for destination folder", end = "")
+            if (self.destination_Flag) : 
+                print(" [SET]", end = "")
+            print("\n2. Choose file path for source folder", end = "")
+            if (self.source_Flag) : 
+                print(" [SET]", end = "")
+            print("\n3. Return to main menu\n")
 
-        #TODO Ask if this directory is correct 
+            # Try/Except for user input 
+            try : 
+                userInput = input()
+                if (userInput not in ["1", "2", "3"]) : 
+                    raise ValueError
+            except(ValueError) : 
+                print("Error: Please enter a number listed in the menu\n")
+                input("Press Enter to continue...")
+                continue
+        
+            # Choose backup folder destination
+            if (userInput == "1") :
+                # If destination folder has already been set, prompt if user wants to update location  
+                if (self.destination_Flag) : 
+                    while(True) : 
+                        print("\nDestination folder has already been set. Change destination directory location? Y/N")
+                        try : 
+                            destInput = input()
+                            if (destInput not in ["y", "Y", "n", "N"]) : 
+                                raise ValueError 
+                        except (ValueError) :
+                            print("Error: Please enter either Y or N")
+                            continue 
+                        # User wants to update location 
+                        if (destInput == "y" or destInput == "Y") : 
+                            self.destination = self.setupLocation("destination", "/Backup/")
+                            break
+                        # User doesn't want to update location -- return to setup menu 
+                        else : 
+                            break 
+                # If destination folder hasn't already been set, call setupLocation
+                else : 
+                    self.destination = self.setupLocation("destination", "/Backup/")
+            
+            # Choose source folder location
+            # TODO - ADD MULTIPLE PATHS / REMOVE PATHS TO SOURCE 
+            if (userInput == "2") : 
+                self.source = self.setupLocation("source", "")
 
-        # Choose source folder location 
-        input("\nPress Enter to choose file path for source folder")
-        self.source = self.getPath("")
-
-        print("Target source directory: ", self.source)
-
-        input("\nPress Enter to return to the main menu...")
+            # Return to main menu
+            if (userInput == "3") : 
+                break 
 
     # Method that grabs file path -- used in setup 
     def getPath(self, dirFolder) : 
@@ -51,10 +96,27 @@ class Backup() :
         tkWin = tk.Tk()
         tkWin.wm_attributes("-topmost", 1) # Move window to front
         tkWin.withdraw() 
-
         # Open file dialog to retrieve path 
         path = filedialog.askdirectory() + dirFolder
-
         # Destroy tkinter window, and return path
         tkWin.destroy()
         return path 
+
+    # Method used to set location in setup
+    def setupLocation(self, type, locStr) : 
+        print("Press Enter to choose file path for", type, "folder")
+        input()
+        location = self.getPath(locStr)
+
+        # TODO -- Check for errors 
+        print("Target", type, "directory:", location)
+
+        # Update flags, then return 
+        if (type == "destination") : 
+            if (not self.destination_Flag) : 
+                self.destination_Flag = True
+        else : 
+            if (not self.source_Flag) : 
+                self.source_Flag = True
+        input("Press Enter to continue...\n")
+        return location
